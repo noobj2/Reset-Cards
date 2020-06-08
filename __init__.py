07139070c2ff91eab8d7ed3f7bb55cdb40bc76f1
noobj2 <63994740+noobj2@users.noreply.github.com>
2020-06-09 00:05:21 +0430

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,11 +3,11 @@ from aqt.utils import showInfo, askUser
 from aqt.qt import *
 
 def accept():
-    reset = askUser("<div style='font-size: 16px'> Reset all cards to new?<br><font color=red>This action can't be undone.</font></div>".format(user_ease), defaultno=True, title="Reset Cards")
+    reset = askUser("<div style='font-size: 16px'> Reset all cards to new?<br><font color=red>This action can't be undone.</font></div>", defaultno=True, title="Reset Cards")
     if reset:
         anki_ease = 0
         mw.col.db.execute("update cards set queue = ?", anki_ease)
-        showInfo("Done".format(user_ease), title="Reset Cards")
+        showInfo("Done", title="Reset Cards")
     else:
         pass
 
